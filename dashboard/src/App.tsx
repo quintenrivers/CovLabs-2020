f9188ee1f01a9dc7114ab9af988e072368f8a1a9
Quinten Rivers <quintendrivers@gmail.com>
2020-05-03 12:35:19 -0400

    placeholder components

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React     from 'react';
 
-import Header from './components/Header';
-import Chart from './components/Chart';
+import Grid      from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Paper     from '@material-ui/core/Paper';
+
+import Header    from './components/Header';
+import Chart     from './components/Chart';
 
 function App() {
   return (
     <div>
       <Header />
-      <Chart />
+      <Container maxWidth='lg'>
+        <Grid container spacing={3}>
+
+          {/* Chart */}
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper>
+              <Chart />
+            </Paper>
+          </Grid>
+
+          {/* Today */}
+          <Grid item xs={12} md={4} lg={3}>
+            <Paper>
+              <p>Today's Performance</p>
+            </Paper>
+          </Grid>
+
+          {/* Recent Visitors */}
+          <Grid item xs={12}>
+            <Paper>
+              <p>Recent Visitors</p>
+            </Paper>
+          </Grid>
+
+        </Grid>
+      </Container>
     </div>
   );
 }
--- dashboard/src/components/Chart.tsx
+++ dashboard/src/components/Chart.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import { useTheme } from '@material-ui/core/styles';
+import { ResponsiveContainer } from 'recharts';
+
+import Title from './Title';
+
 export default function Chart() {
+  const theme = useTheme();
+
   return (
-    <p>Chart</p>
+    <React.Fragment>
+      <Title>This Week</Title>
+      <ResponsiveContainer>
+        <p>Insert Chart</p>
+      </ResponsiveContainer>
+    </React.Fragment>
   );
 }
